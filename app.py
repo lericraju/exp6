eea45cd099e6d5a88b490b58cc5281af61efadce
LERICRAJU <lericraju@gmail.com>
2026-02-24 09:20:31 +0530

    Appid added.

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app= Flask(__name__)
 @app.route("/")
 def index():
 	return"<h1>Leric Raju</h1><br><h2>Appid=2409308</h2>"
+@app.route("/resume")
+def resume():
+	return render_template("resume.html")
 if __name__ =="__main__":
 	app.run(host="0.0.0.0",port=5001,debug=True)
