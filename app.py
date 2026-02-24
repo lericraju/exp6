2a4383c818919c1ab067cdd59f83fa45f0a443cf
LERICRAJU <lericraju@gmail.com>
2026-02-24 09:25:39 +0530

    Appid added.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def index():
-	return"<h1>Leric Raju</h1><br><h2>Appid=2409308</h2>"
+    return "<h1>Leric Raju</h1><br><h2>Appid=2409308</h2>"
+
 @app.route("/resume")
 def resume():
-	return render_template("resume.html")
-if __name__ =="__main__":
-	app.run(host="0.0.0.0",port=5001,debug=True)
+    return render_template("resume.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
